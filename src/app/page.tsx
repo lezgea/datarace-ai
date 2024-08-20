a5032668a0c617d3d43f9b5d79c2eec5e905ac62
muslimragimov <muslim.ragimov@yahoo.com>
2024-08-20 15:56:17 -0400

    Changed clipRule and fillRule keys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -238,7 +238,7 @@ const Home: React.FC = () => {
                         <form className="group relative w-[400px] mt-10">
                             <div className="relative">
                                 <svg width="20" height="20" fill="currentColor" className="absolute left-3 mt-3 text-slate-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
-                                    <path fillRule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
                                 </svg>
                                 <input className="focus:ring-2 focus:ring-blue-500 h-[45px] focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-xl py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Search..." placeholder="Search..." />
                             </div>
