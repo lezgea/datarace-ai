396c1e81a33bbb029ab6a7c8cf1ac5cfd514001f
muslimragimov <muslim.ragimov@yahoo.com>
2024-08-20 21:55:53 -0400

    Change svg import ways. Replaced them by named imports

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,39 @@
+
+
+import DataraceLogo from './datarace-logo.svg';
+import HumanRight from './human-right.svg';
+import TeamBrainStorming from './team-brainstorming.svg';
+import RaceIcon from './all_races.svg';
+import ArrowStyledIcon from './arrow_styled.svg';
+import DrawIcon from './draw.svg';
+import DropIcon from './drop.svg';
+import EducationIcon from './education.svg';
+import EmailIcon from './email.svg';
+import EnvironmentIcon from './environment.svg';
+import EyeClosedIcon from './eye_closed.svg';
+import EyeIcon from './eye.svg';
+import FoundationIcon from './foundation.svg';
+import GoogleIcon from './google.svg';
+
+
+export {
+    // logos
+    DataraceLogo,
+
+    // illustrations
+    HumanRight,
+    TeamBrainStorming,
+
+    // icons
+    RaceIcon,
+    ArrowStyledIcon,
+    DrawIcon,
+    DropIcon,
+    EducationIcon,
+    EmailIcon,
+    EnvironmentIcon,
+    EyeClosedIcon,
+    EyeIcon,
+    FoundationIcon,
+    GoogleIcon,
+}
