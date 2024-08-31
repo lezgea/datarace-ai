5d03de69a7d67f50d43d54a79683254bc145a91f
muslimragimov <muslim.ragimov@yahoo.com>
2024-08-31 18:32:19 -0400

    Divider snapshot tests have been added

--- src/components/shared/__tests__/divider.test.tsx
+++ src/components/shared/__tests__/divider.test.tsx
@@ -5,7 +5,7 @@ import Divider from '../divider';
 
 describe('Divider Component', () => {
     it('should render with default props', () => {
-        const { container } = render(<Divider />);
+        const { container, asFragment } = render(<Divider />);
         const divider = container.firstChild;
 
         expect(divider).toHaveClass('w-full my-2 border-t');
@@ -13,36 +13,44 @@ describe('Divider Component', () => {
             borderColor: 'gray-100',
             borderTopWidth: '1px',
         });
+
+        expect(asFragment()).toMatchSnapshot();
     });
 
     it('should render with custom color', () => {
-        const { container } = render(<Divider color="red" />);
+        const { container, asFragment } = render(<Divider color="red" />);
         const divider = container.firstChild;
 
         expect(divider).toHaveStyle({
             borderColor: 'red',
         });
+
+        expect(asFragment()).toMatchSnapshot();
     });
 
     it('should render with custom thickness', () => {
-        const { container } = render(<Divider thickness="2px" />);
+        const { container, asFragment } = render(<Divider thickness="2px" />);
         const divider = container.firstChild;
 
         expect(divider).toHaveStyle({
             borderTopWidth: '2px',
         });
+
+        expect(asFragment()).toMatchSnapshot();
     });
 
     it('should render with custom marginY', () => {
-        const { container } = render(<Divider marginY="my-4" />);
+        const { container, asFragment } = render(<Divider marginY="my-4" />);
         const divider = container.firstChild;
 
         expect(divider).toHaveClass('w-full my-4 border-t');
+
+        expect(asFragment()).toMatchSnapshot();
     });
 
     it('should apply all custom props', () => {
-        const { container } = render(
-            <Divider color="blue" thickness = "3px" marginY = "my-6" />
+        const { container, asFragment } = render(
+            <Divider color="blue" thickness="3px" marginY="my-6" />
         );
         const divider = container.firstChild;
 
@@ -51,5 +59,7 @@ describe('Divider Component', () => {
             borderColor: 'blue',
             borderTopWidth: '3px',
         });
+
+        expect(asFragment()).toMatchSnapshot();
     });
 });
