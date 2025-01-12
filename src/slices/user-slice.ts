a8a5126d14b0783a013a28615fd80617a4f779eb
lezgea <muslim.ragimov@yahoo.com>
2025-01-11 21:59:49 -0500

    Potential security risks

--- src/slices/user-slice.ts
+++ src/slices/user-slice.ts
@@ -140,10 +140,10 @@ const userSlice = createSlice({
 
                     // Store the token in the cookie with the appropriate expiration time
                     Cookies.set('dtr-token', token, {
-                        httpOnly: true,
+                        // httpOnly: true,
                         secure: process.env.NODE_ENV === 'production',
                         expires: expirationTime, // Expiration time in days
-                        sameSite: 'Strict',
+                        sameSite: 'Lax',
                     });
                 }
             )
