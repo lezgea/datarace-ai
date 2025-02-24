a5f6d46a4b30a93513d3ee2b09fed64ceca5caa1
lezgea <muslim.ragimov@yahoo.com>
2025-02-23 20:58:23 -0500

    Login page's translations issue has been fixed

--- src/utils/withProtectedRoute.tsx
+++ src/utils/withProtectedRoute.tsx
@@ -3,18 +3,20 @@ import { useRouter } from 'next/navigation';
 import { useAuthenticate } from 'hooks/use-auth';
 import { Loader } from '@components/shared';
 import Cookies from 'js-cookie';
+import { useLocale } from 'next-intl';
 
 
 const withProtectedRoute = (WrappedComponent: React.ComponentType) => {
     const ProtectedRoute = (props: any) => {
         const router = useRouter();
+        const lng = useLocale();
         let isAuthenticated = Cookies.get('dtr-token')
 
         useAuthenticate(); // This will dispatch and set the auth state
 
         React.useEffect(() => {
             if (!isAuthenticated) {
-                router.push('/en/sign-in');
+                router.push(`/${lng}/sign-in`);
             }
         }, [isAuthenticated, router]);
 
